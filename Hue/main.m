8ebeac73e607ac6093f8c333c09dde7bbba2613e
David Mazza <github@dmzza.com>
2013-06-29 08:34:35 -0400

    Initial Commit

---
+++ Hue/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hue
+//
+//  Created by David Mazza on 6/29/13.
+//  Copyright (c) 2013 David Mazza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HueTests/HueTests.m
@@ -0,0 +1,36 @@
+//
+//  HueTests.m
+//  HueTests
+//
+//  Created by David Mazza on 6/29/13.
+//  Copyright (c) 2013 David Mazza. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HueTests : XCTestCase
+
+@end
+
+@implementation HueTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
